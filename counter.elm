03a36dab06c287a2f581052c8ea1b294cd36b2db
Christophe Combelles <ccomb@free.fr>
2016-11-12 22:35:21 +0100

    basic styling with MDL

--- counter.elm
+++ counter.elm
@@ -1,6 +1,7 @@
-import Html exposing (div, Html, button, text)
+import Html exposing (div, Html, button, text, Attribute)
 import Html.App as App
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (classList)
 
 -- MODEL
 
@@ -24,13 +25,23 @@ update msg model =
 
 -- VIEW
 
+mdlButtonClass : Attribute Msg
+mdlButtonClass = 
+    classList [
+       ("mdl-button", True),
+       ("mdl-js-button", True),
+       ("mdl-button--raised", True),
+       ("mdl-js-ripple-effect", True),
+       ("mdl-button--accent", True)
+    ]
+
 view : Model -> Html Msg
 view model =
     div [] [
-        button [ onClick Increment ] [text "+"]
+        button [ onClick Increment, mdlButtonClass] [text "+"]
         , div [] [text (toString model.counter)]
-        , button [onClick Decrement] [text "-"]
-        , button [onClick Reset] [text "reset"]
+        , button [onClick Decrement, mdlButtonClass] [text "-"]
+        , button [onClick Reset, mdlButtonClass] [text "reset"]
     ]
 
 main = App.beginnerProgram { model = model, update = update, view = view }
