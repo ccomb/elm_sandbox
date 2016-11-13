977831da6aa8013dd1a51e326da33121004ca9e3
Christophe Combelles <ccomb@free.fr>
2016-11-13 20:30:37 +0100

    use a nice MDL layout

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ test $name || echo "give the elm file as an argument"
 test $name || exit 0
 elm make $name.elm --output main.js
 ssh cody "rm -rf $targetdir && mkdir -p $targetdir"
-rsync -Pr index.html main.js cody:/srv/web/sandbox/elm/$name/
+rsync -Pr index.html main.js styles.css images cody:/srv/web/sandbox/elm/$name/
 ssh cody sed -i "s/Main/$name/" $targetdir/index.html
