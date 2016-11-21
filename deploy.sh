95c4293fcc5910bbcd322ba7cc30a637a0b3159e
Christophe Combelles <ccomb@free.fr>
2016-11-21 23:37:34 +0100

    move assets

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ targetdir=/srv/web/sandbox/elm/$name
 
 test $name || echo "give the elm file as an argument"
 test $name || exit 0
-elm make $name.elm --output main.js
+elm make $name.elm --output static/$name.js
 ssh cody "rm -rf $targetdir && mkdir -p $targetdir"
-rsync -Pr index.html main.js styles.css images cody:/srv/web/sandbox/elm/$name/
+rsync -Pr index.html static/$name.js static/styles.css static/images cody:/srv/web/sandbox/elm/$name/
 ssh cody sed -i "s/Main/$name/" $targetdir/index.html
